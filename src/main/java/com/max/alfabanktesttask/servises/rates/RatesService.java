c988b9681b1020f3c965c7c436c8cc68d3d07411
maxim-creator <max47166978@gmail.com>
2021-01-20 17:46:54 +0300

    change DataTime to LocalDate

--- src/main/java/com/max/alfabanktesttask/servises/rates/RatesService.java
+++ src/main/java/com/max/alfabanktesttask/servises/rates/RatesService.java
@@ -1,11 +1,15 @@
 package com.max.alfabanktesttask.servises.rates;
 
 import com.max.alfabanktesttask.MapConverter;
+import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 
 @Service
+@Data
 public class RatesService {
     CurrentExchangeRate currentExchangeRate;
     YesterdayExchangeRate yesterdayExchangeRate;
@@ -18,7 +22,7 @@ public class RatesService {
 
     public boolean isCurrentRateHigher(String rate){
         Map<String, Double> values = mapConverter.toStringDoubleMap((Map<String, ? extends Comparable<?>>) currentExchangeRate.getCurrentRate().get("rates"));
-        Map<String, Double> yesterdayValues = mapConverter.toStringDoubleMap((Map<String, ? extends Comparable<?>>) yesterdayExchangeRate.getYesterdayRate(YesterdayDate.getYesterdayDate()).get("rates"));
+        Map<String, Double> yesterdayValues = mapConverter.toStringDoubleMap((Map<String, ? extends Comparable<?>>) yesterdayExchangeRate.getYesterdayRate(LocalDate.now().minus(1, ChronoUnit.DAYS).toString()).get("rates"));
         if(values.get(rate) >= yesterdayValues.get(rate))
             return true;
         else
--- src/test/java/com/max/alfabanktesttask/controllers/TestMainController.java
+++ src/test/java/com/max/alfabanktesttask/controllers/TestMainController.java
@@ -1,31 +1,23 @@
 package com.max.alfabanktesttask.controllers;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.extension.ExtendWith;
+import com.max.alfabanktesttask.servises.rates.CurrentExchangeRate;
+import com.max.alfabanktesttask.servises.rates.RatesService;
+import com.max.alfabanktesttask.servises.rates.YesterdayExchangeRate;
+import org.apache.http.client.UserTokenHandler;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.junit.jupiter.api.Assertions;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.security.PublicKey;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class TestMainController {
 
-    WireMockServer wireMockServer = new WireMockServer(8070);
 
-    @BeforeAll
-    public void startMockServer(){
-        wireMockServer.start();
-    }
-
-    @AfterAll
-    public void stopMockServer(){
-        wireMockServer.stop();
-    }
-
-
+    @Test
+    public void testRatesService(){
+    };
 
 }
